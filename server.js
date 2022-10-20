eb0a9592ee9728bd3cadaf73d21cd7dc250bbb92
Quentin Lambert <lambert.quentin96@gmail.com>
2022-10-20 12:27:20 +0200

    heroku: env var

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(process.env.PORT_SERVER, () =>
+app.listen(process.env.PORT || 5000, () =>
   console.log("Serveur en cours d'utilisation...")
 );
 
